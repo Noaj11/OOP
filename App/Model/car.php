8734d345695e709aae2bdd80b4bc0ce16414a924
Md Muntahin Akbar Nobo <mdmuntahinakbarnobo@gmail.com>
2026-05-01 21:28:32 +0600

    Update car.php

--- App/Model/car.php
+++ App/Model/car.php
@@ -1,1 +1,26 @@
+<?php
+// laravel-app/app/Models/Car.php
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model {
+    use HasFactory;
+
+    protected $fillable = ['brand', 'model', 'year', 'color', 'price'];
+
+    protected $casts = [
+        'year' => 'integer',
+        'price' => 'decimal:2'
+    ];
+
+    public function scopeElectric($query) {
+        return $query->where('brand', 'like', '%Tesla%')
+                    ->orWhere('brand', 'like', '%Rivian%');
+    }
+
+    public function getFullNameAttribute() {
+        return $this->brand . ' ' . $this->model;
+    }
+}
