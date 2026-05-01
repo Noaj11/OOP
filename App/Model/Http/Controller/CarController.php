578ebe5a4373b414a26a6df58192b34aaa87180e
Md Muntahin Akbar Nobo <mdmuntahinakbarnobo@gmail.com>
2026-05-01 21:31:27 +0600

    Update CarController.php

--- App/Model/Http/Controller/CarController.php
+++ App/Model/Http/Controller/CarController.php
@@ -1,1 +1,22 @@
+<?php
+// laravel-app/app/Http/Controllers/CarController.php
+namespace App\Http\Controllers;
 
+use App\Models\Car;
+use Illuminate\Http\Request;
+
+class CarController extends Controller {
+    public function index() {
+        $cars = Car::latest()->get();
+        return view('cars.index', compact('cars'));
+    }
+
+    public function store(Request $request) {
+        $car = Car::create($request->validated());
+        return redirect()->route('cars.index')->with('success', 'Car added!');
+    }
+
+    public function apiCars() {
+        return Car::electric()->get();
+    }
+}
