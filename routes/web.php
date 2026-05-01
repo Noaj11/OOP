ea0a1607249fcc28a0033f7253736f0404907aaa
Md Muntahin Akbar Nobo <mdmuntahinakbarnobo@gmail.com>
2026-05-01 21:37:29 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,7 @@
+<?php
+// laravel-app/routes/web.php
+use App\Http\Controllers\CarController;
 
+Route::get('/cars', [CarController::class, 'index'])->name('cars.index');
+Route::post('/cars', [CarController::class, 'store']);
+Route::get('/api/cars', [CarController::class, 'apiCars']);
